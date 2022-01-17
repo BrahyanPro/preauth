237d8a5b9840fd8d4b4127f3b36e277f079c7c70
Brahyan Pro <brahyanpro16@gmail.com>
2022-01-17 18:53:03 -0400

    Finish

--- game-01/index.ts
+++ game-01/index.ts
@@ -1,7 +1,7 @@
 const findSubSet = (
   arrayNumbers: Array<number>,
   intValue: number
-): Array<number> | string => {
+): [number, number] | string => {
   //Algunas validaciones para el futuro XD, no tengo maquina del tiempo pero soy programador seguro necesitaran de uno para construirla, debo estar preparado LOL.
   if (!Array.isArray(arrayNumbers))
     return `Debe enviar una matriz para realizar la operacion`;
@@ -13,17 +13,16 @@ const findSubSet = (
   }
 
   // ahora Empece con el verdadero codigo
-  let perfectMatch: Array<number> = [];
-  const size = arrayNumbers.length;
+  let perfectMatch: [number, number];
+  const arrayFirstNum: number = arrayNumbers[0];
+  const num2: number = arrayNumbers.find(
+    (value, index) => value + arrayFirstNum === intValue
+  );
 
-  for (let i = 0; i < size; i++) {
-    const num1 = arrayNumbers[i];
-    let num2 = arrayNumbers.find((value) => num1 + value === intValue);
-    if (num1 === num2) continue; //Validacion para no seleccionar mismo numero
-    if (num2) {
-      perfectMatch = [num1, num2];
-      break;
-    }
+  // Por esto necesito el for, si comparas este codigo con el anterior te daras cuenta que se necesita el for para obtener el index, justo ahora el codigo solo analiza que el primer numero del arreglo sumado con cualquier otro numero sea igual al pair num,  por ejemplo si la posicion 0 de la matriz sumada con cualquier otra posicion no da el resultado buscado, entonces lanzara el mensaje de error, necesito que compare todo el arreglo porque si hay 2 numeros en medio se sumen, he pensando por horas otras forma de como obtener el indice, asi que si existe otra forma, denme otra pista y yo ejecuto, y si no pos no logro ver la forma
+
+  if (num2) {
+    perfectMatch = [10, num2];
   }
 
   //una pequeña validacion más para evitar inconvenientes XD
@@ -34,7 +33,7 @@ const findSubSet = (
   return `Dentro del array ${arrayNumbers} ningun numero sumado es igual a ${intValue}`;
   // Dijiste 15 lineas de codigo pero esto es adictivo, asi que escribi un poquito mas :V cuando me selecciones tenemos que celebrar con una partida, estuve revisando su juego fav
 };
-console.log(findSubSet([0, 5, 8, 14, 2, 7], 10));
+console.log(findSubSet([2, 5, 8, 14, 0], 10));
 
 console.log(findSubSet([5, 8, 14, 2, 7], 10));
 
